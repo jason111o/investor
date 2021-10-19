d21e2c72e7a863d8755f5ab4b68058a6835530f7
Jason Pippin <razrjlinux@gmail.com>
2021-10-19 04:35:05 -0500

    Added main function and advanced other functions

--- source/investor.sh
+++ source/investor.sh
@@ -1,31 +1,45 @@
 #!/usr/bin/env bash
 
-#### Vars
-title="Pip's Investment Calculator"
-text="Fill in the entries to calculate growth of wealth"
-version=2.0
-warning="zenity --error --title $title"
-case $1 in
-  "-v")
-  echo -e "\n$title\n$version\n"
-  exit 0
-  ;;
-esac
+#### MAIN ####
+function main() {
+	# Global vars
+	title="Pip's Investment Calculator"
+	text="Fill in the entries to calculate growth of wealth"
+	version=2.1
+	warning="zenity --error --title $title"
 
-################################################################################
-###################################FUNCTIONS####################################
+	cli_options
+
+	continue=0
+	while [[ $continue == 0 ]]; do
+		investment_calculator
+		continue=$?
+	done
+}
+
+#### CLI options
+function cli_options() {
+	case $1 in
+		"-v")
+		echo -e "\n$title\n$version\n"
+		exit 0
+		;;
+	esac
+}
+
 #### Confirming numerical values
-num_check() {
+function num_check() {
   for (( i=0; i<${#1}; i++ )); do
     if [[ ${1:$i:1} != [0-9] ]]; then
 			$warning --text "Enter whole numbers only"
-			exit 1
+			return 1
     fi
   done
 }
+
 #### Aquire investment strategy from user
-#### e.g. investing_growth [initial_investment|rate_of_return|years_to_invest|monthly_contribution|yearly_contribution]
-investing_growth() {
+#### e.g. investment_growth [initial_investment|rate_of_return|years_to_invest|monthly_contribution|yearly_contribution]
+function investment_growth() {
 	# $1
 	if [[ $1 == "" ]]; then
 		initial_investment=0
@@ -58,12 +72,6 @@ investing_growth() {
 	else
 		yearly_contribution=$(echo $5)
 	fi
-	echo "****TESTING****
-	initial_investment = \"$initial_investment\"
-	yearly_rate = \"$yearly_rate\"
-	years_to_invest = \"$years_to_invest\"
-	monthly_contribution = \"$monthly_contribution\"
-	yearly_contribution = \"$yearly_contribution\""
 	if [[ $monthly_contribution > 0 ]]; then
 		total_months=$(echo "$years_to_invest * 12" | bc -l)
 		montly_rate=$(echo "$yearly_rate / 12" | bc -l)
@@ -79,7 +87,6 @@ investing_growth() {
 	fi
 	if [[ $years_to_invest > 0 ]]; then
 		for (( i=0; i<$years_to_invest; i++ )); do
-			echo "i = $i" #*testing*#
 			growth=$(echo "$initial_investment * $yearly_rate + $yearly_contribution" | bc -l)
 			initial_investment=$(echo "$initial_investment + $growth" | bc -l)
 		done
@@ -89,36 +96,42 @@ investing_growth() {
 	fi
   zenity --info --title "Pip's Investment Calculator" --text "Total Investment Growth \$$result"
 }
-###################################FUNCTIONS####################################
-################################################################################
 
-#### Get basic investment data
-investment_data=$(zenity --forms \
-	--title "$title" \
-	--text $text \
-	--add-entry="Initial Investment $" \
-	--add-entry="Rate of Return %" \
-	--add-entry="Years Planned To Invest" \
-	--add-entry="Monthly Contribution $" \
-	--add-entry="Yearly Contribution $")
-if [[ $? != 0 ]]; then
-	exit 99
-fi
+#### Get the data from user and calculate it
+function investment_calculator() {
+	investment_data=$(zenity --forms \
+		--title "$title" \
+		--text $text \
+		--add-entry="Initial Investment $" \
+		--add-entry="Rate of Return %" \
+		--add-entry="Years Planned To Invest" \
+		--add-entry="Monthly Contribution $" \
+		--add-entry="Yearly Contribution $")
+	if [[ $? != 0 ]]; then
+		exit 99
+	fi
+	# Split up the data into vars
+	initial_investment=$(echo $investment_data | cut -d \| -f 1)
+	rate_of_return=$(echo $investment_data | cut -d \| -f 2)
+	years_to_invest=$(echo $investment_data | cut -d \| -f 3)
+	monthly_contribution=$(echo $investment_data | cut -d \| -f 4)
+	yearly_contribution=$(echo $investment_data | cut -d \| -f 5)
+	# Verify numerical values
+	num_check $initial_investment
+	if [[ $? == 1 ]]; then investment_calculator; fi
+	num_check $rate_of_return
+	if [[ $? == 1 ]]; then investment_calculator; fi
+	num_check $years_to_invest
+	if [[ $? == 1 ]]; then investment_calculator; fi
+	num_check $monthly_investment
+	if [[ $? == 1 ]]; then investment_calculator; fi
+	num_check $yearly_investment
+	if [[ $? == 1 ]]; then investment_calculator; fi
+	# Run the function "investment_growth" on the given data
+	investment_growth $initial_investment $rate_of_return $years_to_invest $monthly_contribution $yearly_contribution
+}
 
-#### Seperate data values
-initial_investment=$(echo $investment_data | cut -d \| -f 1)
-rate_of_return=$(echo $investment_data | cut -d \| -f 2)
-years_to_invest=$(echo $investment_data | cut -d \| -f 3)
-monthly_contribution=$(echo $investment_data | cut -d \| -f 4)
-yearly_contribution=$(echo $investment_data | cut -d \| -f 5)
-
-#### Validate data values
-num_check $initial_investment
-num_check $rate_of_return
-num_check $years_to_invest
-num_check $monthly_investment
-num_check $yearly_investment
-
-investing_growth $initial_investment $rate_of_return $years_to_invest $monthly_contribution $yearly_contribution
+#### Run "main function"
+main
 
 exit 0
